90379194265438ba61c2e75d0247b1f461805e14
Uriel Campos <urielfcampos95@gmail.com>
2025-07-23 16:13:14 +0100

    Remove configurable export_limit

--- erts/emulator/beam/export.h
+++ erts/emulator/beam/export.h
@@ -37,10 +37,6 @@
 #define OP_PAD
 #endif
 
-#define EXPORT_INITIAL_SIZE   4000
-#define EXPORT_MAX_SIZE        (2048*1024)
-#define EXPORT_LIMIT          (512*1024)
-
 typedef struct export_
 {
     /* !! WARNING !!
